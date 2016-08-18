225da70e97a6d2eca94ee4ec4bcbcb014d90fb7b
David Sommer <david.sommer.ch@gmail.com>
2016-08-18 15:13:38 +0200

    Add files via upload

---
+++ src/ch/dasoft/colorutil/ColorUtil.java
@@ -0,0 +1,155 @@
+package ch.dasoft.colorutil;
+
+import java.awt.Color;
+
+public class ColorUtil {
+
+  /** <p style="background-color: rgb(  0,   0,   0);"><br/><br/></p> */
+  public static final Color BLACK               = Color.BLACK;
+
+  /** <p style="background-color: rgb(255, 255, 255);"><br/><br/></p> */
+  public static final Color WHITE               = Color.WHITE;
+
+  /** <p style="background-color: rgb(106, 168,  79);"><br/><br/></p> */
+  public static final Color GREEN                    = new Color(106, 168,  79);
+  /** <p style="background-color: rgb(153, 204,   0);"><br/><br/></p> */
+  public static final Color GREEN_LIGHT              = new Color(153, 204,   0);
+  /** <p style="background-color: rgb(  0, 128,  64);"><br/><br/></p> */
+  public static final Color GREEN_DARK               = new Color(  0, 128,  64);
+  /** <p style="background-color: rgb(182, 215, 168);"><br/><br/></p> */
+  public static final Color GREEN_PASTELL_LIGHT      = new Color(182, 215, 168);
+  /** <p style="background-color: rgb(99, 184, 139);"><br/><br/></p> */
+  public static final Color GREEN_PASTELL_DARK       = new Color( 99, 184, 139);
+  /** <p style="background-color: rgb(68, 77, 60);"><br/><br/></p> */
+  public static final Color OLIV                     = new Color( 68, 77,  60);
+  /** <p style="background-color: rgb(106, 97, 78);"><br/><br/></p> */
+  public static final Color TARMAC                   = new Color(106,  97,  78);
+
+  /** <p style="background-color: rgb(255, 136,   0);"><br/><br/></p> */
+  public static final Color ORANGE                   = new Color(255, 136,   0);
+  /** <p style="background-color: rgb(255, 187,  51);"><br/><br/></p> */
+  public static final Color ORANGE_LIGHT             = new Color(255, 187,  51);
+  /** <p style="background-color: rgb(213,  68,  50);"><br/><br/></p> */
+  public static final Color ORANGE_DARK              = new Color(213,  68,  50);
+  /** <p style="background-color: rgb(246, 178, 107);"><br/><br/></p> */
+  public static final Color ORANGE_PASTELL           = new Color(246, 178, 107);
+  /** <p style="background-color: rgb(252, 208, 158);"><br/><br/></p> */
+  public static final Color ORANGE_PASTELL_LIGHT      = new Color(252, 208, 158);
+
+  /** <p style="background-color: rgb(240, 236,  53);"><br/><br/></p> */
+  public static final Color YELLOW_LIGHT             = new Color(240, 236,  53);
+  /** <p style="background-color: rgb(247, 242, 244);"><br/><br/></p> */
+  public static final Color YELLOW_LIGHT_2           = new Color(247, 242, 224);
+  /** <p style="background-color: rgb(247, 248, 224);"><br/><br/></p> */
+  public static final Color YELLOW_LIGHT_3           = new Color(247, 248, 224);
+  /** <p style="background-color: rgb(253, 253, 169);"><br/><br/></p> */
+  public static final Color YELLOW_PASTELL           = new Color(253, 253, 169);
+
+  /** <p style="background-color: rgb(119, 136, 153);"><br/><br/></p> */
+  public static final Color SILBER                   = new Color(119, 136, 153);
+
+  /** <p style="background-color: rgb(153, 101, 21);"><br/><br/></p> */
+  public static final Color GOLD                     = new Color(153, 101, 21);
+
+  /** <p style="background-color: rgb(204,   0,   0);"><br/><br/></p> */
+  public static final Color RED                      = new Color(204,   0,   0);
+  /** <p style="background-color: rgb(255,  68,  68);"><br/><br/></p> */
+  public static final Color RED_LIGHT                = new Color(255,  68,  68);
+  /** <p style="background-color: rgb(232, 114, 124);"><br/><br/></p> */
+  public static final Color RED_PASTELL              = new Color(232, 114, 124);
+  /** <p style="background-color: rgb(236, 159, 164);"><br/><br/></p> */
+  public static final Color RED_PASTELL_LIGHT         = new Color(236, 159, 164);
+
+  /** <p style="background-color: rgb(128,   0,  64);"><br/><br/></p> */
+  public static final Color MAGENTA                  = new Color(128,   0,  64);
+  /** <p style="background-color: rgb(126,  81,  101);"><br/><br/></p> */
+  public static final Color MAGENTA_PASTELL          = new Color(126, 81, 101);
+
+  /** <p style="background-color: rgb(153,  51, 204);"><br/><br/></p> */
+  public static final Color VIOLET                   = new Color(153,  51, 204);
+  /** <p style="background-color: rgb(170, 102, 204);"><br/><br/></p> */
+  public static final Color VIOLET_LIGHT             = new Color(170, 102, 204);
+  /** <p style="background-color: rgb( 90,  51, 128);"><br/><br/></p> */
+  public static final Color VIOLET_DARK              = new Color(90,  51, 128);
+  /** <p style="background-color: rgb(213, 158, 240);"><br/><br/></p> */
+  public static final Color VIOLET_PASTELL_LIGHT     = new Color(213, 158, 240);
+
+  /** <p style="background-color: rgb(211  ,33, 132);"><br/><br/></p> */
+  public static final Color PINK                     = new Color(211  ,33, 132);
+
+  /** <p style="background-color: rgb(  0, 153, 204);"><br/><br/></p> */
+  public static final Color BLUE                     = new Color(0,   153, 204);
+  /** <p style="background-color: rgb( 51, 181, 229);"><br/><br/></p> */
+  public static final Color BLUE_LIGHT               = new Color(51,  181, 229);
+  /** <p style="background-color: rgb( 58,  71, 140);"><br/><br/></p> */
+  public static final Color BLUE_DARK                = new Color(58,  71, 140);
+  /** <p style="background-color: rgb(108, 151, 213);"><br/><br/></p> */
+  public static final Color BLUE_PASTELL             = new Color(108, 151, 213);
+  /** <p style="background-color: rgb( 43,  99, 182);"><br/><br/></p> */
+  public static final Color BLUE_DARK_PASTELL       = new Color( 43,  99, 182);
+  /** <p style="background-color: rgb(237, 243, 254);"><br/><br/></p> */
+  public static final Color BLUE_LIGHT_CORIX         = new Color(237, 243, 254);
+  /** <p style="background-color: rgb(153, 187, 239);"><br/><br/></p> */
+  public static final Color BLUE_PASTELL_LIGHT       = new Color(153, 187, 239);
+
+  /** <p style="background-color: rgb(184, 184, 180);"><br/><br/></p> */
+  public static final Color GRAY_DARK                = new Color(184, 184, 180);
+  /** <p style="background-color: rgb(220, 220, 220);"><br/><br/></p> */
+  public static final Color GRAY                     = new Color(220,220,220);
+  /** <p style="background-color: rgb(242, 242, 242);"><br/><br/></p> */
+  public static final Color GRAY_LIGHT               = new Color(242, 242, 242);
+  /** <p style="background-color: rgb(131, 143, 157);"><br/><br/></p> */
+  public static final Color GRAY_CORIX               = new Color(131, 143, 157);
+  /** <p style="background-color: rgb(215, 220, 226);"><br/><br/></p> */
+  public static final Color GRAY_LIGHT_CORIX         = new Color(215, 220, 226);
+  /** <p style="background-color: rgb(212, 212, 212);"><br/><br/></p> */
+  public static final Color GRAY_BACKGROUND_CORIX    = new Color(212, 212, 212);
+  /** <p style="background-color: rgb(232, 232, 232);"><br/><br/></p> */
+  public static final Color GRAY_BACKGROUND_LIGHT_CORIX    = new Color(232, 232, 232);
+
+  /** <p style="background-color: rgb(244, 209, 153);"><br/><br/></p> */
+  public static final Color BEIGE                    = new Color(244, 209, 153);
+  /** <p style="background-color: rgb(253, 237, 185);"><br/><br/></p> */
+  public static final Color BEIGE_LIGHT              = new Color(253, 237, 185);
+  /** <p style="background-color: rgb(246, 178, 107);"><br/><br/></p> */
+  public static final Color BEIGE_DARK               = new Color(246, 178, 107);
+
+  /** <p style="background-color: rgb(142,  43,  48);"><br/><br/></p> */
+  public static final Color BRAUN                    = new Color(142,  43,  48);
+
+  /** <p style="background-color: rgb(  0,  99, 165);"><br/><br/></p> */
+  public static final Color CONNECTION_LABEL_RS = new Color(  0,  99, 165);
+  /** <p style="background-color: rgb(235, 151,  30);"><br/><br/></p> */
+  public static final Color CONNECTION_LABEL_FI = new Color(235, 151,  30);
+  /** <p style="background-color: rgb(235, 151,  30);"><br/><br/></p> */
+  public static final Color CONNECTION_LABEL_WS = new Color(170, 102, 204);
+  public static final Color CONNECTION_LABEL_LD = BLUE_PASTELL;
+  public static final Color CONNECTION_LABEL_YC = ORANGE_PASTELL;
+  public static final Color CONNECTION_LABEL_SC = VIOLET_PASTELL_LIGHT;
+  public static final Color CONNECTION_LABEL_HP = VIOLET;
+
+  public static String convertRgbToHex(Color color) {
+    String hex = Integer.toHexString(color.getRGB() & 0xffffff);
+    if (hex.length() < 6) {
+        hex = "0" + hex;
+    }
+    return "#" + hex;
+  }
+
+  public static Color convertHexToColor(String hex) {
+    return new Color(Integer.parseInt(hex, 16));
+  }
+
+  public static boolean isHell(Color c) {
+
+    double rot = (double)c.getRed() / 255;
+    double gruen = (double)c.getGreen() / 255;
+    double blau = (double)c.getBlue() / 255;
+
+    double helligkeit = (0.2126 * rot) + (0.7152 * gruen) + (0.0722 * blau);
+
+    return helligkeit * 255 > 127;
+
+  }
+
+}
